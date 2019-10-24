b07c6150f5138094afbd183317a4e5ab24ac3f29
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-10-24 23:50:02 +0530

    feat: Comment line reminder

--- routes/quiz.js
+++ routes/quiz.js
@@ -29,6 +29,7 @@ const generateQuestionList = (lastRandomQ, totalQuestions) => shuffle(range(1, l
     .concat(range(lastRandomQ, totalQuestions + 1));
 
 router.post('/start', async (req, res) => {
+    // TODO: Authenticate participant
     const participant = await Participant.findOne({
         email: req.session.email,
     });
