d95ddbfb034c0841ff74909bb4f19b39604c816a
thebongy <rishit.bansal0@gmail.com>
2019-11-17 15:59:49 +0530

    fix: Seperate DB Name and DB URL

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/CCS', {
+mongoose.connect('${process.env.MONGO_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
 }, (err) => {
