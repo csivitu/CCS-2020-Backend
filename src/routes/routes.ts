a1d68f1ae68e4e8385a37b756bf94e685d7e3595
ashikka <ashikagupta28@gmail.com>
2020-12-05 17:34:18 +0530

    fix: Route paths

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,22 @@
-/* eslint-disable import/extensions */
 import express from 'express';
-import controllers from '../controllers/controllers';
+import controllers from 'controllers/controllers';
 
 const router = express.Router();
+// TODO: Why error here
+router
+	.route('/end')
+	.post(controllers.endRoute);
 
 router
-	.route('/')
-	.get(controllers.domainRoute)
-	.post(controllers.endRoute)
+	.route('/domains')
+	.get(controllers.domainRoute);
+
+router
+	.route('/start')
 	.post(controllers.startRoute);
 
-module.exports = router;
+router
+	.route('/respond')
+	.post(controllers.respondRoute);
+
+export default router;
