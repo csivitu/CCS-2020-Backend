209c4d9666e0202e8a3973a404d9f9db4514109b
ashikka <ashikagupta28@gmail.com>
2020-12-05 17:32:46 +0530

    feat: Added router

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 /* eslint-disable no-console */
+import express from 'express';
+import bodyParser from 'body-parser';
 import './utils/env.ts';
 import './models/db.ts';
-import express from 'express';
+import mainRoutes from 'routes/routes';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/', mainRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on ${PORT}`);
